6a7bd46aa5978f6facb52d3bc400f94a8fbe05d1
Lasha Ghurtskaia <lashagurtskaia33@gmail.com>
2025-07-03 11:25:30 +0400

    Update Student.java

--- oop/finalexam/t2/Student.java
+++ oop/finalexam/t2/Student.java
@@ -9,8 +9,11 @@ public class Student {
     String name = "Lasha";
     String lastName = "Ghurtskaia";
     List<Course> courses = Arrays.asList(
-            new Course("Mathematics", "None", "Algebra, Geometry"),
-            new Course("Physics", "Mathematics", "Mechanics, Optics")
+            new Course("Calculus 2 (ENG)", "Calculus 1 (ENG)", "Antiderivatives. Definite integrals. Techniques and applications of integration. Improper integrals. Infinite series."),
+            new Course("Mathematical Foundation of computing (ENG)", "CS50, Calculus 1 (ENG)", "Mathematical Logic; Elements of Discrete Mathematics; Elements of Sets Theory; Elements of Graph Theory; Elements of Combinatorics; Elements of Digital Systems; "),
+            new Course("English Language Course C1-2 (ENG)", "English Language Course C1-1 (ENG)", "None specified in sylabus"),
+            new Course("Computer Organization (ENG)", "CS50", "Representing and manipulating information; Machine-level representations of programs; Optimizing program performance. The memory hierarchy."),
+            new Course("Object Oriented Programming (ENG)", "CS50", "Java syntax and data structures; Procedural programming; Classes; Encapsulation, polymorphism, inheritance; Packages; Working with the network; Work with files; Working with text data; Work with the terminal."),
     );
 
     public Student(String name, String lastName, List<Course> courses) {
